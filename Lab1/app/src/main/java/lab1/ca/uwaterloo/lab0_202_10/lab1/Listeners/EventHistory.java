9fc15abff1c53e2caee19421e0e8a9c8ab4b9035
Mackenzie Goodwin <MackenzieJamesGoodwin@gmail.com>
2017-05-30 10:09:47 -0400

    Added files to gitignore

--- Lab1/app/src/main/java/lab1/ca/uwaterloo/lab0_202_10/lab1/Listeners/EventHistory.java
+++ Lab1/app/src/main/java/lab1/ca/uwaterloo/lab0_202_10/lab1/Listeners/EventHistory.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.LinkedList;
-import java.util.List;
 
 /**
  * This class consists of a event history recorder. Also has the ability to write
