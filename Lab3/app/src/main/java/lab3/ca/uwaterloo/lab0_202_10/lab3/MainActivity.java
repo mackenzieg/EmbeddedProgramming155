63c590a46821597b7b1a19c787ad471182d7546b
Mackenzie Goodwin <mackenziejamesgoodwin@gmail.com>
2017-06-18 22:57:45 -0400

    Change cache as soon as master has changed

--- Lab3/app/src/main/java/lab3/ca/uwaterloo/lab0_202_10/lab3/MainActivity.java
+++ Lab3/app/src/main/java/lab3/ca/uwaterloo/lab0_202_10/lab3/MainActivity.java
@@ -1,12 +1,13 @@
 package lab3.ca.uwaterloo.lab0_202_10.lab3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.wtw.BuiltDevice;
 import com.wtw.Device;
 import com.wtw.compression.MeanCompressor;
 import com.wtw.detectors.DefaultGestureDetector;
+import com.wtw.distance.AbsoluteDistance;
 import com.wtw.distance.EuclideanDistance;
 import com.wtw.event.EventHandler;
 import com.wtw.event.EventListener;
@@ -15,36 +16,55 @@ import com.wtw.event.events.PostTimeWarpEvent;
 import com.wtw.event.events.StartTimeWarpEvent;
 import com.wtw.filters.DifferenceEquivalenceFilter;
 import com.wtw.filters.LowPassFilter;
+import com.wtw.timeseries.TimeSeries;
+import com.wtw.timewarp.SlowTimeWarpCalculator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final List<TimeSeries> recordedGestures = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BuiltDevice builtDevice = new Device()
-                .addCompressor(new MeanCompressor(5))
-                .addFilter(new LowPassFilter(1))
+        final AtomicInteger numCaptured = new AtomicInteger(0);
+
+        final BuiltDevice builtDevice = new Device()
+                .addCompressor(new MeanCompressor())
+                .addFilter(new LowPassFilter(3))
                 .addFilter(new DifferenceEquivalenceFilter(3))
                 .setGestureDetector(new DefaultGestureDetector(new EuclideanDistance()))
-//                .addTimeWarpCalculator(new SlowTimeWarpCalculator(new AbsoluteDistance()))
-                .registerListener(new EventListener() {
-                    @EventHandler
-                    public void getCompressed(PostCompressionEvent postCompressionEvent) {
-                        System.out.println("After");
-                        System.out.println(postCompressionEvent.getAfter().toString());
-                    }
+                .addTimeWarpCalculator(new SlowTimeWarpCalculator(new AbsoluteDistance()))
+                .build().setStartCompression(true);
 
-                    @EventHandler
-                    public void postTimeWarp(PostTimeWarpEvent postTimeWarpEvent) {
-                        System.out.println(postTimeWarpEvent.getDistance());
+        builtDevice.getEventBus().register(new EventListener() {
+            @EventHandler
+            public void getCompressed(PostCompressionEvent postCompressionEvent) {
+                if (numCaptured.getAndIncrement() <= 3) {
+                    recordedGestures.add(postCompressionEvent.getAfter());
+                } else {
+                    if (!builtDevice.getTimeWarpManager().isStarted()) {
+                        builtDevice.setStartTimeWarp(true);
                     }
+                }
+            }
 
-                    @EventHandler
-                    public void compare(StartTimeWarpEvent startTimeWarpEvent) {
+            @EventHandler
+            public void postTimeWarp(PostTimeWarpEvent postTimeWarpEvent) {
+                System.out.println(postTimeWarpEvent.getDistance());
+            }
 
-                    }
-                }).build().setStartCompression(true).setStartTimeWarp(true);
+            @EventHandler
+            public void compare(StartTimeWarpEvent startTimeWarpEvent) {
+                for (TimeSeries timeSeries : recordedGestures) {
+                    startTimeWarpEvent.addComparison(timeSeries);
+                }
+            }
+        });
     }
 }
