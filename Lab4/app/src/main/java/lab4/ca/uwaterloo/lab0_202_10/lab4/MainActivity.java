dc20ba23a625624a1aa031508bb709b86771ca77
Mackenzie Goodwin <mackenziejamesgoodwin@gmail.com>
2017-07-05 22:53:47 -0400

    Starting lab4

---
+++ Lab4/app/src/main/java/lab4/ca/uwaterloo/lab0_202_10/lab4/MainActivity.java
@@ -0,0 +1,15 @@
+package lab4.ca.uwaterloo.lab0_202_10.lab4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // https://github.com/channguyen/2048-android
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
