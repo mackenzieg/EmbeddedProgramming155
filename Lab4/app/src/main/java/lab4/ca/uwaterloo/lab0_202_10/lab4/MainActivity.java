47d09b4d6fc335a5ff47361908775bde3b84bec9
Mackenzie Goodwin <MackenzieJamesGoodwin@gmail.com>
2017-07-13 09:06:26 -0400

    Increase tick rate

--- Lab4/app/src/main/java/lab4/ca/uwaterloo/lab0_202_10/lab4/MainActivity.java
+++ Lab4/app/src/main/java/lab4/ca/uwaterloo/lab0_202_10/lab4/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
         final Timer myTimer = new Timer();
         final GameLoopTask gameLoop = new GameLoopTask(this, getApplicationContext(), view);
-        myTimer.schedule(gameLoop, 30, 40);
+        myTimer.schedule(gameLoop, 30, 30);
 
         gestureIndicator.setText("Recording " + labelledGestures.get(index.getAndIncrement()).getDirection().getLabel() + "\n");
 
